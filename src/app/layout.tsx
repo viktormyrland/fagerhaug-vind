42be52477df5a27a0c2bf1cbdf05848475019345
Viktor Myrland <viktormyrland@gmail.com>
2024-04-23 11:16:20 +0200

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "~/styles/globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
@@ -20,7 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans ${inter.variable}`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
