ea752a77c4d16a64bc96e81484436bd965b27ed6
Viktor Myrland <viktormyrland@gmail.com>
2025-08-05 13:23:27 +0200

    remove vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import "~/styles/globals.css";
-
-import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import { Suspense } from "react";
 
@@ -24,7 +22,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
         <Suspense>{children}</Suspense>
-        <Analytics />
       </body>
     </html>
   );
