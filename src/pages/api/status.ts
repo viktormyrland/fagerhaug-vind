f5aad5b38deffa4a984dd36985f1c5cbc784f043
Viktor Myrland <viktormyrland@gmail.com>
2025-12-26 12:21:13 +0100

    lower error threshold for /api/status

--- src/pages/api/status.ts
+++ src/pages/api/status.ts
@@ -1,27 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getWindData } from '~/server/WindDataActions';
- 
+
 type ResponseData = {
   message: string
 }
- 
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
 
-    getWindData(60)
-        .then((wd) => {
-            if(wd.wind_histogram.length == 0 || wd.maxGust.direction == undefined){
-          res.status(500).json({message: "Empty response from database (last 10 minutes)."})
-            }else{
+  getWindData(10)
+    .then((wd) => {
+      if (wd.wind_histogram.length == 0 || wd.maxGust.direction == undefined) {
+        res.status(500).json({ message: "Empty response from database (last 10 minutes)." })
+      } else {
 
-  res.status(200).json({ message: 'OK' })
-            }
-        })
-        .catch((e) => {
-          console.log(e);
+        res.status(200).json({ message: 'OK' })
+      }
+    })
+    .catch((e) => {
+      console.log(e);
 
-          res.status(500).json({message: "Could not retrieve data from database."})
-        });
+      res.status(500).json({ message: "Could not retrieve data from database." })
+    });
 }
